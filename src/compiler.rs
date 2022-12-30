f4f671d5ae9a4bccbf5ca1a28bd9a7832fb4810f
Dexter Hill <49513414+DexterHill0@users.noreply.github.com>
2022-12-30 22:58:58 +0000

    lots of changes (spawn + move trigger working)

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,201 +1,131 @@
+use ahash::AHashMap;
 use itertools::Itertools;
 
-use crate::object::*;
+use std::fmt::Write;
 
-use std::fmt::{Display, Formatter, Result};
-
-/*
-let name = obj_props! {
-    1 OBJ_ID;
-    71 FOLLOW # 1 => 1347;
-    71 CENTER # 1 => 1346;
-    71 TARGET_POS # 1 => 901;
+use crate::{
+    object::{ObjectParams, ObjectType},
+    processing::{ContextKey, Tree},
 };
-*/
 
-macro_rules! obj_props {
-    ($key:ident; $params:expr; $($id:literal $(# ($($m:literal)*))? : $x:ident,)*) => {
-        match $key {
-            $(
-                $id $(
-                    if [$($m,)*].contains(
-                        match $params.get(&1).unwrap_or(&ObjParam::Int(0)) {
-                            ObjParam::Int(v) => v,
-                            _ => unreachable!()
-                        }
-                    )
-                )? => stringify!($x).to_string(),
-            )*
-            _ => format!("{}", $key),
-        }
-    };
+// #[derive(Default)]
+// struct TriggerInfo {
+//     pub targets: Vec<isize>,
+// }
+
+#[derive(Default)]
+pub struct Compiler<'c> {
+    tree: Tree<'c>,
 }
 
-impl Display for GDObj {
-    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
-        let mut string = String::new();
+impl<'c> Compiler<'c> {
+    pub fn compile(&mut self, tree: Tree<'c>) -> String {
+        self.tree = tree;
 
-        string.push_str("$.add(obj {\n");
+        let mut out = String::new();
 
-        use ObjParam::*;
+        // TODO: output comments?
+        self.compile_static_objects(&mut out);
 
-        let definition = self.params.keys().sorted().map(|key| {
-            let param = &self.params[key];
+        self.compile_context_0(&mut out);
+        self.compile_other_contexts(&mut out);
 
-            let key_string = obj_props!{
-                key; self.params;
-                1: OBJ_ID,
-                2: X,
-                3: Y,
-                4: HORIZONTAL_FLIP,
-                5: VERTICAL_FLIP,
-                6: ROTATION,
-                7: TRIGGER_RED,
-                8: TRIGGER_GREEN,
-                9: TRIGGER_BLUE,
-                10: DURATION,
-                11: TOUCH_TRIGGERED,
-                13: PORTAL_CHECKED,
-                15: PLAYER_COLOR_1,
-                16: PLAYER_COLOR_2,
-                17: BLENDING,
-                20: EDITOR_LAYER_1,
-                21: COLOR,
-                22: COLOR_2,
-                23: TARGET_COLOR,
-                24: Z_LAYER,
-                25: Z_ORDER,
-                28: MOVE_X,
-                29: MOVE_Y,
-                30: EASING,
-                31: TEXT,
-                32: SCALING,
-                34: GROUP_PARENT,
-                35: OPACITY,
-                36: ACTIVE_TRIGGER,
-                41: HVS_ENABLED,
-                42: COLOR_2_HVS_ENABLED,
-                43: HVS,
-                44: COLOR_2_HVS,
-                45: FADE_IN,
-                46: HOLD,
-                47: FADE_OUT,
-                48: PULSE_HSV,
-                49: COPIED_COLOR_HVS,
-                50: COPIED_COLOR_ID,
-                51: TARGET,
-                52: TARGET_TYPE,
-                54: YELLOW_TELEPORTATION_PORTAL_DISTANCE,
-                56: ACTIVATE_GROUP,
-                57: GROUPS,
-                58: LOCK_TO_PLAYER_X,
-                59: LOCK_TO_PLAYER_Y,
-                60: COPY_OPACITY,
-                61: EDITOR_LAYER_2,
-                62: SPAWN_TRIGGERED,
-                63: SPAWN_DURATION,
-                64: DONT_FADE,
-                65: MAIN_ONLY,
-                66: DETAIL_ONLY,
-                67: DONT_ENTER,
-                68: ROTATE_DEGREES,
-                69: TIMES_360,
-                70: LOCK_OBJECT_ROTATION,
-                71 # (901): TARGET_POS,
-                71 # (1346): CENTER,
-                71 # (1347): FOLLOW,
-                71: TARGET_POS,
-                72: X_MOD,
-                73: Y_MOD,
-                75: STRENGTH,
-                76: ANIMATION_ID,
-                77: COUNT,
-                78: SUBTRACT_COUNT,
-                79: PICKUP_MODE,
-                80 # (1816): BLOCK_A,
-                80: BLOCK_A,
-                80: ITEM,
-                81: HOLD_MODE,
-                82: TOGGLE_MODE,
-                84: INTERVAL,
-                85: EASING_RATE,
-                86: EXCLUSIVE,
-                87: MULTI_TRIGGER,
-                88: COMPARISON,
-                89: DUAL_MODE,
-                90: SPEED,
-                91: DELAY,
-                92: Y_OFFSET,
-                93: ACTIVATE_ON_EXIT,
-                94: DYNAMIC_BLOCK,
-                95: BLOCK_B,
-                96: GLOW_DISABLED,
-                97: ROTATION_SPEED,
-                98: DISABLE_ROTATION,
-                104: COUNT_MULTI_ACTIVATE,
-                100: USE_TARGET,
-                101: TARGET_POS_AXES,
-                102: EDITOR_DISABLE,
-                103: HIGH_DETAIL,
-                105: MAX_SPEED,
-                106: RANDOMIZE_START,
-                107: ANIMATION_SPEED,
-                108: LINKED_GROUP,
-            };
+        out
+    }
 
-            format!(
-                "  {}: {},",
-                key_string,
-                match param {
-                    Bool(val) => val.to_string(),
-                    Float(val) => val.to_string(),
-                    Int(val) => val.to_string(),
-                    Ints(val) => format!(
-                        "[{}]",
-                        val
-                            .iter()
-                            .map(|v| v.to_string() + "g")
-                            .collect::<Vec<String>>()
-                            .join(", ")
-                    ),
-                    Text(val) => {
-                        let b64 = base64::decode(val).map(String::from_utf8);
+    // context 0 is the base context and triggers
+    // can have delays based on player speed so any triggers in context 0
+    // are added as raw objects
+    fn compile_context_0(&mut self, out: &mut String) {
+        // TODO:
+        // `obj_ids.triggers`?
+        // general trigger functions?,
+        for trigger in &self.tree.context.pop(0).triggers {
+            out.push_str("$.add(obj {\n");
 
-                        if b64.is_ok() && b64.as_ref().unwrap().is_ok() && !val.is_empty() {
-                            format!("$.b64encode({})", quote_string(&b64.unwrap().unwrap()))
-                        } else {
-                            // technically this is unreachable, but I'll let it in anyways
-                            // (we could easily .unwrap() above without needing to check if it's valid or not)
-                            quote_string(val)
-                        }
-                    },
-                }
-            )
-        }).collect::<Vec<String>>().join("\n");
+            let object = self.tree.objects[*trigger];
 
-        string.push_str(&definition);
+            write!(out, "{}\n}})\n\n", compile_object(object)).unwrap();
+        }
+    }
 
-        string.push_str("\n})\n");
+    fn compile_other_contexts(&self, out: &mut String) {
+        let mut target_vars = AHashMap::new();
 
-        write!(f, "{}", string)
+        for target in self.tree.targets.iter().sorted() {
+            let target_var = format!("target_{0}", target);
+            let target_str = format!("{} = {}g\n", &target_var, target);
+
+            target_vars.insert(target, format!("target_{0}", target));
+            out.push_str(&target_str);
+        }
+
+        // TODO: parent contexts - separate structs?
+        // context 0 is skipped as it was popped earlier
+        for (_, context) in &self.tree.context.contexts {
+            writeln!(out, "\ncontext_{} = !{{", context.group).unwrap();
+
+            if let Some(delay) = context.delay {
+                writeln!(out, "    wait({})", delay).unwrap();
+            }
+
+            for trig in &context.triggers {
+                // it will always be a trigger
+                let trigger = self.tree.objects[*trig].get_trigger();
+
+                writeln!(
+                    out,
+                    "    {}.{}",
+                    target_vars.get(&trigger.target_group().unwrap()).unwrap(),
+                    trigger.to_function()
+                )
+                .unwrap();
+            }
+
+            write!(out, "{}}}\ncontext_{}!\n", "", context.group).unwrap();
+        }
+    }
+
+    fn compile_static_objects(&self, out: &mut String) {
+        // TODO: use `target_x` vars (if available) inside GROUPS property?
+        for object in self.tree.static_objects.iter() {
+            out.push_str("$.add(obj {\n");
+
+            let object = self.tree.objects[*object];
+
+            write!(out, "{}\n}})\n\n", compile_object(object)).unwrap();
+        }
     }
 }
 
-pub fn compile(objects: Vec<GDObj>) -> String {
-    objects
-        .iter()
-        .map(|object|{
-            format!("{}\n", object)
+fn compile_object(object: &ObjectType) -> String {
+    let params = match object {
+        ObjectType::Block(b) => &b.params,
+        ObjectType::Trigger(t) => &t.params,
+    };
+
+    let definition = params
+        .keys()
+        .sorted()
+        .map(|key| {
+            let param = &params[key];
+
+            // TODO: decode base64 string params?
+            let param_name = ObjectParams::param_name(*key, object.id());
+
+            format!("    {}: {}", param_name, param.to_string())
         })
         .collect::<Vec<String>>()
-        .join("\n")
+        .join("\n");
+
+    definition
 }
 
-pub fn quote_string(string: &str) -> String {
-    // this makes sense, I swear
-    if string.contains('\'') {
-        format!("\"{}\"", string.replace('\"', "\\\""))
-    } else {
-        format!("\'{}\'", string.replace('\'', "\\\'"))
-    }
-}
+// fn quote_string(string: &str) -> String {
+//     // this makes sense, I swear
+//     if string.contains('\'') {
+//         format!("\"{}\"", string.replace('\"', "\\\""))
+//     } else {
+//         format!("\'{}\'", string.replace('\'', "\\\'"))
+//     }
+// }
---
+++ src/trigger.rs
@@ -0,0 +1,129 @@
+use num_derive::FromPrimitive;
+
+use crate::object::{ObjectParams, Params};
+
+macro_rules! trigger_fns {
+    (
+        [$name:ident]
+
+        $(
+            $trig_name:ident => $fn_name:ident(
+                $($params:tt)*
+            )
+        )*
+    ) => {
+        pub struct $name {
+            form: TriggerType,
+        }
+
+        impl $name {
+            pub fn with_params(&self, params: &Params) -> String {
+                match self.form {
+                    $(
+                        TriggerType::$trig_name => {
+                            format!(
+                                "{}( {})",
+                                stringify!($fn_name),
+                                trigger_fns!(@genparams [params] $($params)*)
+                            )
+                        },
+                    )*
+
+                    _ => todo!()
+                }
+            }
+        }
+
+        impl From<TriggerType> for $name {
+            fn from(t: TriggerType) -> $name {
+                $name {
+                    form: t,
+                }
+            }
+        }
+    };
+
+    (@genparams [$params:ident] $param_name:ident $($rest:tt)*) => {{
+        let mut args = match $params.get(&ObjectParams::Id:: $param_name ) {
+            Some(ObjectParams::Params:: $param_name (val)) => {
+                format!("{}, ", val.to_string())
+            },
+            _ => {
+                trigger_fns!(@defaultval $($rest)*)
+            }
+        };
+
+        args.push_str(
+            &trigger_fns!(@genparams [$params] $($rest)*)
+        );
+
+        args
+    }};
+
+    // `?` and everything following
+    // ex: the `?, Y` in `X?, Y`
+    (@genparams [$params:ident] ? $($rest:tt)*) => {
+        trigger_fns!(@genparams [$params] $($rest)*)
+    };
+
+    // `,` and everything following
+    // ex: the `, Y` in `X, Y`
+    (@genparams [$params:ident] , $($rest:tt)*) => {
+        trigger_fns!(@genparams [$params] $($rest)*)
+    };
+
+    // no params left
+    (@genparams [$params:ident] ) => {
+        ""
+    };
+
+    // default values
+    // parameter is not optional (no `?`)
+    (@defaultval , $($rest:tt)*) => {
+        panic!("trigger missing param")
+    };
+
+    // parameter is optional (has `?`)
+    (@defaultval ?, $($rest:tt)*) => {
+        "".to_string()
+    };
+}
+
+trigger_fns! {
+    [TriggerFunctions]
+
+    Move => move(
+        X,
+        Y,
+        DURATION?,
+        EASING?,
+        EASING_RATE?,
+    )
+}
+
+#[derive(FromPrimitive, Copy, Clone, Debug)]
+pub enum TriggerType {
+    Spawn = 1268,
+    Move = 901,
+    // TODO: rest of triggers
+}
+
+#[derive(Clone, Debug)]
+pub struct Trigger {
+    pub params: Params,
+    pub form: TriggerType,
+}
+
+impl Trigger {
+    // TODO: target different in different triggers (pulse trigger, etc)
+    pub fn target_group(&self) -> Option<isize> {
+        match self.params.get(&ObjectParams::Id::TARGET) {
+            Some(ObjectParams::Params::TARGET(t)) => Some(*t),
+            _ => None,
+        }
+    }
+
+    pub fn to_function(&self) -> String {
+        TriggerFunctions::from(self.form).with_params(&self.params)
+    }
+}
